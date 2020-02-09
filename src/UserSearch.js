f91a0e640a8d71e36b36e1ba8031d133fd021e29
ine12362324 <nabaraj.saha@tesco.com>
2020-02-09 23:33:44 +0530

    first commit

---
+++ src/UserSearch.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class UserSearch extends Component {
+    constructor(props){
+        super(props);
+    }
+    componentDidMount(){
+
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default UserSearch;
